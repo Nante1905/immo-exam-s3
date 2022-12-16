c090c6d4d1f5b208bed2d5bc2b65b958799fa449
Nante <nantemino15@gmail.com>
2022-12-16 08:50:20 +0300

    metier et requete sql login

---
+++ src/php/login.php
@@ -0,0 +1,32 @@
+<?php
+require_once("./functions.php");
+
+if(!isset($_POST["email"], $_POST["mdp"])) {
+    $res = array(
+        "logres" => "not logged"
+    );
+
+    echo json_encode($res);
+}
+else {
+    $email = $_POST["email"];
+    $mdp = $_POST["mdp"];
+
+    $logged = login($email, $mdp);
+
+    if($logged) {
+        $res = array(
+            "logres" => "logged"
+        );
+    
+        echo json_encode($res);
+    }
+    else {
+        $res = array(
+            "logres" => "not logged"
+        );
+    
+        echo json_encode($res);
+    }
+
+}
